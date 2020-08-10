9c8736386fa7fd0d04dcbc76372330753d08e517
zuberol <zuber.kuba@gmail.com>
2020-08-10 21:53:49 +0200

    initial commit

---
+++ src/main/java/pl/jzuber/songmate/SongmateApplication.java
@@ -0,0 +1,13 @@
+package pl.jzuber.songmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jzuber/songmate/SongmateApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jzuber.songmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
